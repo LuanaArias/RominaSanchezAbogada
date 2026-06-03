63f5cd13cb75c6cb85a8948ab90d0e3fc2e5be3e
Luana <luanaariaaas@gmail.com>
2026-06-03 16:17:13 -0300

    modificando contactos

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -37,6 +37,16 @@ export function Footer() {
             <span>+54 11 6751 2241</span>
           </a>
 
+          <a 
+            href="https://mail.google.com/mail/?view=cm&fs=1&to=drasanchezro@gmail.com"
+            className="email-link"
+          >
+            <svg width="20" height="16" viewBox="0 0 20 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M20 2C20 0.9 19.1 0 18 0H2C0.9 0 0 0.9 0 2V14C0 15.1 0.9 16 2 16H18C19.1 16 20 15.1 20 14V2ZM18 2L10 7L2 2H18ZM18 14H2V4L10 9L18 4V14Z" fill="currentColor"/>
+            </svg>
+            <span>drasanchezro@gmail.com</span>
+          </a>
+
         </div>
 
       </div>
